6558766fe52252b1b8ad4a5fa410973206ee0144
Laika May <chinamaykzalaika@gmail.com>
2021-02-24 06:51:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 describe('index', () => {
   // there's stuff in here, too
 });
+
+
+const error = console.error("HALP!");
+console.log(error);
+
+const log = console.log("I would be a logger."); // get it?
+console.log(log);
+
+const warn = console.warn("Danger!");
+console.log(warn);
